575357713438fecbd352c4c3a879c8dcb85d08fc
chdsayeed <870826>
2019-11-15 15:49:25 +0800

    审批链

--- eladmin-system/src/main/java/com/fn/modules/documents/domain/AuditChain.java
+++ eladmin-system/src/main/java/com/fn/modules/documents/domain/AuditChain.java
@@ -5,17 +5,18 @@ import com.fn.modules.system.domain.User;
 import lombok.Data;
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.bean.copier.CopyOptions;
+
 import javax.persistence.*;
 import java.sql.Timestamp;
 import java.io.Serializable;
 
 /**
-* @author jie
-* @date 2019-11-11
-*/
+ * @author jie
+ * @date 2019-11-11
+ */
 @Entity
 @Data
-@Table(name="audit_chain")
+@Table(name = "audit_chain")
 public class AuditChain implements Serializable {
 
     // 自增主键ID
@@ -24,9 +25,12 @@ public class AuditChain implements Serializable {
     @Column(name = "id")
     private Long id;
 
+    @Column(name = "job_id")
+    private Long jobId;
+
     // 关联岗位
-    @ManyToOne(cascade = {CascadeType.ALL})
-    @JoinColumn(name = "job_id")
+    @ManyToOne(cascade = {CascadeType.REFRESH, CascadeType.DETACH})
+    @JoinColumn(name = "job_id", insertable = false, updatable = false)
     private Job job;
 
     // 审核顺序，从1开始
@@ -38,18 +42,18 @@ public class AuditChain implements Serializable {
     private Integer source;
 
     // 创建时间
-    @Column(name = "create_time",nullable = false)
+    @Column(name = "create_time", nullable = false)
     private Timestamp createTime;
 
     // 更新时间
-    @Column(name = "update_time",nullable = false)
+    @Column(name = "update_time", nullable = false)
     private Timestamp updateTime;
 
     // 删除位（0:未删除,1:已删除）
-    @Column(name = "deleted",nullable = false)
+    @Column(name = "deleted", nullable = false)
     private Integer deleted;
 
-    public void copy(AuditChain source){
-        BeanUtil.copyProperties(source,this, CopyOptions.create().setIgnoreNullValue(true));
+    public void copy(AuditChain source) {
+        BeanUtil.copyProperties(source, this, CopyOptions.create().setIgnoreNullValue(true));
     }
 }
