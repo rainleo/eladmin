0be805f3a6a3225651570286126a9368442c251d
chdsayeed <870826>
2019-11-20 18:21:50 +0800

    附件编辑

--- eladmin-system/src/main/java/com/fn/modules/documents/service/impl/ReimbursementDocumentsServiceImpl.java
+++ eladmin-system/src/main/java/com/fn/modules/documents/service/impl/ReimbursementDocumentsServiceImpl.java
@@ -2,8 +2,10 @@ package com.fn.modules.documents.service.impl;
 
 import com.fn.modules.documents.domain.ApplicationDocuments;
 import com.fn.modules.documents.domain.DocumentReviewer;
+import com.fn.modules.documents.domain.ReimbursementDetail;
 import com.fn.modules.documents.domain.ReimbursementDocuments;
 import com.fn.modules.documents.repository.DocumentReviewerRepository;
+import com.fn.modules.documents.repository.ReimbursementDetailRepository;
 import com.fn.utils.NumGenerator;
 import com.fn.utils.ValidationUtil;
 import com.fn.modules.documents.repository.ReimbursementDocumentsRepository;
@@ -12,13 +14,17 @@ import com.fn.modules.documents.service.dto.ReimbursementDocumentsDTO;
 import com.fn.modules.documents.service.dto.ReimbursementDocumentsQueryCriteria;
 import com.fn.modules.documents.service.mapper.ReimbursementDocumentsMapper;
 import com.fn.utils.twitter.SnowflakeIdUtils;
+import com.google.common.collect.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -41,6 +47,9 @@ public class ReimbursementDocumentsServiceImpl implements ReimbursementDocuments
 
     @Autowired
     private DocumentReviewerRepository documentReviewerRepository;
+
+    @Autowired
+    private ReimbursementDetailRepository reimbursementDetailRepository;
 
     @Override
     public Object queryAll(ReimbursementDocumentsQueryCriteria criteria, Pageable pageable) {
@@ -65,6 +74,8 @@ public class ReimbursementDocumentsServiceImpl implements ReimbursementDocuments
     public ReimbursementDocumentsDTO create(ReimbursementDocuments resources) {
         //生成单据号(时间戳)
         resources.setReimbursementNo(NumGenerator.getNumber());
+        // 附件表先删后增
+        updateReimbursementDetail(resources);
         //入库申请单据表
         return reimbursementDocumentsMapper.toDto(reimbursementDocumentsRepository.save(resources));
     }
@@ -77,6 +88,30 @@ public class ReimbursementDocumentsServiceImpl implements ReimbursementDocuments
         ReimbursementDocuments reimbursementDocuments = optionalReimbursementDocuments.get();
         reimbursementDocuments.copy(resources);
         reimbursementDocumentsRepository.save(reimbursementDocuments);
+        updateReimbursementDetail(resources);
+    }
+
+    /**
+     * 更新附件表
+     * @param resources
+     */
+    private void updateReimbursementDetail(ReimbursementDocuments resources) {
+        // 1.根据附件url删除附件表，然后新增
+        List<ReimbursementDetail> reimbursementDetailList = resources.getReimbursementDetailList();
+        if (reimbursementDetailList == null || reimbursementDetailList.isEmpty()) {
+            return;
+        }
+        // 删除附件
+        reimbursementDetailRepository.deleteInBatch(reimbursementDetailList);
+        // 新增附件
+        reimbursementDetailList.stream().filter(reimbursementDetail -> {
+            reimbursementDetail.setDeleted(0);
+            reimbursementDetail.setReimbursementDocumentsId(resources.getId());
+            return true;
+        }).collect(Collectors.toList());
+        reimbursementDetailRepository.saveAll(reimbursementDetailList);
+        // 2.远程删除七牛云文件 TODO
+
     }
 
     @Override
