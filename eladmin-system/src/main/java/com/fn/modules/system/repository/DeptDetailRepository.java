ce689827a57260a2b3806aec76c9eed0730d6e93
chdsayeed <870826>
2019-11-21 17:54:26 +0800

    编辑附件

--- eladmin-system/src/main/java/com/fn/modules/system/repository/DeptDetailRepository.java
+++ eladmin-system/src/main/java/com/fn/modules/system/repository/DeptDetailRepository.java
@@ -4,9 +4,12 @@ import com.fn.modules.system.domain.DeptDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
+
 /**
 * @author jie
 * @date 2019-11-18
 */
 public interface DeptDetailRepository extends JpaRepository<DeptDetail, Long>, JpaSpecificationExecutor {
+    List<DeptDetail> findByDeptId(Long id);
 }
